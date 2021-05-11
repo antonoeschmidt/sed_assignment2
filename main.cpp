a9e57f4d27e7168aab336416a20667194e2c813c
Oggy <vip12345k@gmail.com>
2021-05-11 12:51:24 +0700

    User

--- main.cpp
+++ main.cpp
@@ -67,26 +67,89 @@ Guest::Guest(string id, string name, string address, string phone)
 bool Guest::isGuest() { return guest; }
 int Guest::getMaxRental() { return maxRental; }
 
-int main(int argc, char const *argv[])
+void Menu()
 {
-
-    // Items
-    Item *item1 = new Item("123", "hej", "book", 1, 100, false);
-    cout << item1->getId() << endl;
-    cout << "isBorrowed: " << boolalpha << item1->getBorrowed() << endl;
-    item1->setBorrowed(true);
-    cout << "isBorrowed: " << boolalpha << item1->getBorrowed() << endl;
-
-    // Customers
-    Customer *cus1 = new Customer("10", "Oggy", "District 12", "0702 602 509");
-    cout << "Is guest?: " << boolalpha << cus1->isGuest() << endl;
-    cus1->setItem(item1);
-    Item *item2 = cus1->getItem();
-    cout << item2->getTitle() << endl;
-
-    Guest *guest1 = new Guest("11", "Doggy", "District 14", "0123 012 234");
-    cout << "Is guest?: " << boolalpha << guest1->isGuest() << endl;
-    cout << "Max rental: " << boolalpha << guest1->getMaxRental() << endl;
-
-    return 0;
+    cout << "Welcome to Oggy's video store" << endl;
+    cout << "Enter an option below." << endl;
+    cout << "1. Add a new item, update or delete an existing item" << endl;
+    cout << "2. Add new customer or update an existing customer" << endl;
+    cout << "3. Promote an existing customer" << endl;
+    cout << "4. Rent an item" << endl;
+    cout << "5. Return an item" << endl;
+    cout << "6. Display all items" << endl;
+    cout << "7. Display out-of-stock items" << endl;
+    cout << "8. Display all customers" << endl;
+    cout << "9. Display group of customers" << endl;
+    cout << "10. Search items or customers" << endl;
+    cout << "Exit" << endl;
+    input();
 }
+
+void input()
+{
+    string choice;
+    int option;
+    cin >> choice;
+    try
+    {
+        option = stoi(choice);
+    }
+    catch (const invalid_argument)
+    {
+        if (choice == "Exit" || choice == "exit")
+        {
+            //Exit function here
+            return;
+        }
+        else
+        {
+            cerr << "Invalid input";
+        }
+    }
+    if (option >= 1 && option <= 10)
+    {
+        switch (option)
+        {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        }
+    }
+    else
+    {
+        cerr << "Invalid Input";
+    }
+}
+
+    int main(int argc, char const *argv[])
+    {
+
+        //UI
+        Menu();
+        // Items
+        Item *item1 = new Item("123", "hej", "book", 1, 100, false);
+        cout << item1->getId() << endl;
+        cout << "isBorrowed: " << boolalpha << item1->getBorrowed() << endl;
+        item1->setBorrowed(true);
+        cout << "isBorrowed: " << boolalpha << item1->getBorrowed() << endl;
+
+        // Customers
+        Customer *cus1 = new Customer("10", "Oggy", "District 12", "0702 602 509");
+        cout << "Is guest?: " << boolalpha << cus1->isGuest() << endl;
+        cus1->setItem(item1);
+        Item *item2 = cus1->getItem();
+        cout << item2->getTitle() << endl;
+
+        Guest *guest1 = new Guest("11", "Doggy", "District 14", "0123 012 234");
+        cout << "Is guest?: " << boolalpha << guest1->isGuest() << endl;
+        cout << "Max rental: " << boolalpha << guest1->getMaxRental() << endl;
+
+        return 0;
+    }
