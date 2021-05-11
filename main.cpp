36286b8fa7cbba35503f7280bdaf40bf89e62bec
Oggy <vip12345k@gmail.com>
2021-05-11 12:54:34 +0700

    buggy

--- main.cpp
+++ main.cpp
@@ -112,16 +112,16 @@ void input()
     {
         switch (option)
         {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
+        case 1: ;
+        case 2: ; 
+        case 3: ;
+        case 4: ; 
+        case 5: ; 
+        case 6: ; 
+        case 7: ; 
+        case 8: ;
+        case 9: ; 
+        case 10: ;
         }
     }
     else
