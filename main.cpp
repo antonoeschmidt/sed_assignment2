d9765c73876e25e5276dfb2ff7cf54f445553d8a
antonoeschmidt <s163053@student.dtu.dk>
2021-05-18 16:48:33 +0700

    Tests

--- main.cpp
+++ main.cpp
@@ -6,59 +6,10 @@
 #include "Customers.cpp"
 #include "Items.h"
 #include "items.cpp"
+#include "FileHandlar.h"
+#include "FileHandlar.cpp"
 
 using namespace std;
-
-/* ------ CUSTOMERS ------ */
-// Customer::Customer() {}
-// Customer::Customer(string id, string name, string address, string phone)
-// {
-//     this->id = id;
-//     this->name = name;
-//     this->address = address;
-//     this->phone = phone;
-//     this->guest = false;
-//     this->noOfReturns = 0;
-// }
-// string Customer::getId() { return id; }
-// string Customer::getName() { return name; }
-// string Customer::getAddress() { return address; }
-// string Customer::getPhone() { return phone; }
-// Item *Customer::getItem() { return testItem; }
-// int Customer::getNoOfReturns() { return noOfReturns; }
-// bool Customer::isGuest() { return guest; }
-// void Customer::setItem(Item *item)
-// {
-//     this->testItem = item;
-// }
-// // ---- Guest -----
-// Guest::Guest() : Customer()
-// {
-//     this->guest = true;
-//     this->maxRental = 2;
-// }
-// Guest::Guest(string id, string name, string address, string phone)
-//     : Customer(id, name, address, phone)
-// {
-//     this->guest = true;
-//     this->maxRental = 2;
-// }
-// bool Guest::borrowItem(/* item here */)
-// {
-//     cout << "Borrow Item called" << endl;
-
-//     return false;
-// }
-
-// bool Guest::returnItem(/* item here */)
-// {
-//     cout << "Return Item called" << endl;
-
-//     return false;
-// }
-
-// bool Guest::isGuest() { return guest; }
-// int Guest::getMaxRental() { return maxRental; }
 
 void input()
 {
@@ -145,8 +96,31 @@ void Test()
     cout << "Is guest?: " << boolalpha << reg1->isGuest() << endl;
 }
 
+void populateItems(vector<Item*> items) {
+
+    // Call fileHandler to read items.txt
+    // which should return an array of string which we will iterate over
+
+    string items[5];
+    for (int i = 0; i < items.size(); i++)
+    {
+        /* code */
+    }
+    
+
+
+
+}
+
 int main(int argc, char *argv[])
 {
+
+    vector<Item*> items;
+    vector<Customer*> customers;
+
+    populateItems(items);
+
+
     // Checks if the program is executed in the correct format
     if (argc != 2)
     {
--- test_vectors.cpp
+++ test_vectors.cpp
@@ -1,71 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "Customers.h"
+#include "Customers.cpp"
 
 using namespace std;
 
-/* ------ CUSTOMERS ------ */
-Customer::Customer() {}
-Customer::Customer(string id, string name, string address, string phone)
-{
-    this->id = id;
-    this->name = name;
-    this->address = address;
-    this->phone = phone;
-    this->guest = false;
-    this->noOfReturns = 0;
-}
-string Customer::getId() { return id; }
-string Customer::getName() { return name; }
-string Customer::getAddress() { return address; }
-string Customer::getPhone() { return phone; }
-Item *Customer::getItem() { return testItem; }
-int Customer::getNoOfReturns() { return noOfReturns; }
-bool Customer::isGuest() { return guest; }
-void Customer::setItem(Item *item)
-{
-    this->testItem = item;
-}
-// ---- Guest -----
-Guest::Guest() : Customer()
-{
-    this->guest = true;
-    this->maxRental = 2;
-}
-Guest::Guest(string id, string name, string address, string phone)
-    : Customer(id, name, address, phone)
-{
-    this->guest = true;
-    this->maxRental = 2;
-}
-bool Guest::borrowItem(/* item here */)
-{
-    cout << "Borrow Item called" << endl;
-
-    return false;
-}
-
-bool Guest::returnItem(/* item here */)
-{
-    cout << "Return Item called" << endl;
-
-    return false;
-}
-
-bool Guest::isGuest() { return guest; }
-int Guest::getMaxRental() { return maxRental; }
-
-
-
 int main()
 {
-    vector<Item*> guestList;
+    vector<Customer*> guestList;
 
-    It *guest1 = new Guest("11", "Doggy", "District 14", "0123 012 234");
-    Guest *guest2 = new Guest("11", "Anton", "District 4", "0123 012 234");
+    Guest *guest1 = new Guest("11", "Guest Doggy", "District 14", "0123 012 234");
+    Regular *reg1 = new Regular("11", "Regular Anton", "District 4", "0123 012 234");
 
     guestList.push_back(guest1);
-    guestList.push_back(guest2);
+    guestList.push_back(reg1);
 
     cout << "Output of begin and end: ";
 
